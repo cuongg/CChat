cfb548152b6a7fb3e2a1fc2f189b185c976d739b
HoCuongg <cuonghv025@gmail.com>
2021-04-16 09:01:11 +0700

    push chat to firebase/database

--- src/screen/ChatScreen/index.tsx
+++ src/screen/ChatScreen/index.tsx
@@ -2,40 +2,33 @@ import React, {useState, useCallback, useEffect} from 'react';
 import {GiftedChat} from 'react-native-gifted-chat';
 import {useSelector} from 'react-redux';
 import {RootState} from 'redux/reducers';
+import database from '@react-native-firebase/database';
+import _ from 'lodash';
+import moment from 'moment';
+
+const NUMBER_MESS = 20;
 
 const ChatScreen = () => {
   const userReducer = useSelector((state: RootState) => state.userReducer);
-  console.log('ChatScreen -> userReducer', userReducer);
 
   const [messages, setMessages] = useState<any[]>([]);
 
   useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello developer',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-        // You can also add a video prop:
-        // Mark the message as sent, using one tick
-        sent: true,
-        // Mark the message as received, using two tick
-        received: true,
-        // Mark the message as pending with a clock loader
-        pending: true,
-        // Any additional custom parameters are passed through
-      },
-    ]);
+    database()
+      .ref('/chat-group')
+      .limitToLast(NUMBER_MESS)
+      .on('child_added', (snapShot) => {
+        const value = snapShot.toJSON() || {};
+        const message = [value];
+        setMessages((previousMessages) =>
+          _.uniqBy(GiftedChat.append(previousMessages, message), '_id'),
+        );
+      });
   }, []);
 
   const onSend = useCallback((message: any[] = []) => {
-    console.log('onSend -> message', message);
-    // message[0].pending = true;
-    message[0].system = true;
+    message[0].createdAt = moment().toJSON();
+    database().ref('/chat-group').push(message[0]);
     setMessages((previousMessages) =>
       GiftedChat.append(previousMessages, message),
     );
@@ -46,9 +39,10 @@ const ChatScreen = () => {
       messages={messages}
       onSend={onSend}
       user={{
-        _id: 1,
+        _id: userReducer.data.uid,
+        name: userReducer.data.phoneNumber,
       }}
-      render
+      renderAvatarOnTop
     />
   );
 };
